b76db736f2ce7b6661bfd3ce290295507211eefb
Ovi <oviice@gmail.com>
2020-12-03 12:30:54 +0600

    Practice

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import "./App.css";
-import { Container,Row,Col } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./component/Header";
 
 function App() {
